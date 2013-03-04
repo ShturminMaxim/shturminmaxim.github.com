cd01015519c93ce750ba59a668b99cbfd92e7916
Sturmin Maxim <koyotka@yahoo.com>
2013-03-04 15:05:24 +0200

    nav-menuchange and Raphael.js graphs

--- menu-animate/js/menu_nav_animation.js
+++ menu-animate/js/menu_nav_animation.js
@@ -1,10 +1,14 @@
 "use strict";
+// This is unoptimized test
 // TODO set negative margin relative to max menu-item width.
-/*global $*/
+// TODO optimize functions
+// TODO add function for animate holders
+
+/*global $, console, alert */
 $(function() {
 
 	(function change_z_indexes() {
-		var z_index = 0;
+		var z_index = 5;
 		$('.menu .menu-item').map(function(val, i) {
 			z_index -= 1;
 			$(i).css({
@@ -14,6 +18,39 @@ $(function() {
 	})();
 
 	(function click_event() {
+
+		$('.menu-item').mousedown(function(event){
+			event.preventDefault();
+
+			if($('.visible-graph')) {
+				$('.visible-graph').animate({
+					'margin-left': '-800'
+				}, 1500);
+				$('.visible-graph').removeClass('visible-graph');
+			}
+
+			var hash = $(event.target).parent().find('a').attr('href');
+			if(hash ==='#1') {
+				$('#holder-1').addClass('visible-graph');
+				$('#holder-1').animate({
+					'margin-left': '0'
+				}, 1000);
+			}
+			if(hash ==='#2') {
+				$('#holder-2').addClass('visible-graph');
+				$('#holder-2').animate({
+					'margin-left': '0'
+				}, 1000);
+			}
+			if(hash ==='#3') {
+				$('#holder-3').addClass('visible-graph');
+				$('#holder-3').animate({
+					'margin-left': '0'
+				}, 1000);
+			}
+
+		});
+
 		$('.first-item').click(function() {
 			$(this).toggleClass('closed');
 			$('.menu-item').stop();
@@ -21,6 +58,12 @@ $(function() {
 				$('.menu-item').animate({
 					'margin-left': '-260'
 				}, 1500);
+				if($('.visible-graph')) {
+				$('.visible-graph').animate({
+					'margin-left': '-800'
+				}, 1500);
+				$('.visible-graph').removeClass('visible-graph');
+				}
 			} else {
 				$('.menu-item').animate({
 					'margin-left': '-30'
@@ -28,5 +71,4 @@ $(function() {
 			}
 		});
 	})();
-
 });
