e67c3cca91b30dda60d6b025d49a16df06509a27
Sturmin Maxim <koyotka@yahoo.com>
2013-03-07 16:38:40 +0200

    test

--- iphone/js/default.js
+++ iphone/js/default.js
@@ -1,5 +1,25 @@
-window.onload = function() {
+var myScroll;
+var mySlide;
+
+function loaded() {
+	myScroll = new iScroll('wrapper');
+
+	mySlide = new iScroll('slider', {
+		snap: true,
+		momentum: false,
+		hScrollbar: false,
+		onScrollEnd: function () {
+			document.querySelector('#indicator > li.active').className = '';
+			document.querySelector('#indicator > li:nth-child(' + (this.currPageX+1) + ')').className = 'active';
+		}
+	});
+}
+
+document.addEventListener('DOMContentLoaded', loaded, false);
+document.addEventListener('touchmove', function (e) { e.preventDefault(); }, false);
+
+/*window.onload = function() {
 	setTimeout(function() {
 	window.scrollTo(0, 1);
 }, 100);
-};
+};*/
